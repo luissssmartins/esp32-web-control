48c326790b13bd0f304ea5c2f44655c826bf3402
Luis Martins <chateadorn@gmail.com>
2023-03-04 00:28:52 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <Arduino.h>
+
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
+
+const char* ssid = "";
+const char* password = "";
+
+const int espLedPin = 2;
+
+int keepAlive = 0;
+
+String ledState;
+
+AsyncWebServer server(80);
+
+String httpPlaceHolderProcessor(const String& val) {
+  Serial.println(val);
+
+  if (val == "STATE") {
+
+    if (digitalRead(espLedPin)) {
+      ledState = "LIGADO";
+    } else {
+      ledState = "DESLIGADO";
+    }
+
+    Serial.println(ledState);
+
+    return ledState;
+  }
+
+  return String();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(espLedPin, OUTPUT);
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Erro ao carregar flash system do ESP32.");
+    return;
+  }
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+
+    Serial.println("Conectando ao wifi...");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  defineRoutes();
+
+  server.begin();
+}
+
+void loop() {
+  keepAlive++;
+}
+
+void defineRoutes() {
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html", String(), httpPlaceHolderProcessor);
+  });
+
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(espLedPin, HIGH);    
+    request->send(SPIFFS, "/index.html", String(), false, httpPlaceHolderProcessor);
+  });
+
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(espLedPin, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, httpPlaceHolderProcessor);
+  });
+}
